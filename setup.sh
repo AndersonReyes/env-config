d3dd4ab7781f398ab4d07108cd6f946141a3c273
Anderson Reyes <reyesanderson428@gmail.com>
2021-03-03 23:59:09 -0500

    update install for bash completion

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@ sudo apt install gnupg2 software-properties-common
 udo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 sudo apt update
-sudo apt install -y git vim tmux gh fzf gcc wget curl xclip xsel
+sudo apt install -y git vim tmux gh gcc wget curl xclip bash-completion
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
